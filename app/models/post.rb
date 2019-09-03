047972ed56bb8024336e6e4c7d1a8e18d3a3bc73
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-09-03 15:01:40 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ActiveRecord::Base
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
   validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
-  validates :clickbait_title, on: :title
+  validate :clickbait_title, on: :title
 
   def clickbait_title
     if title.include?("Won't Believe", "Secret", "Top[number]", "Guess")
